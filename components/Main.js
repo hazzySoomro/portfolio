bff886503333165441c2c8e717d38c713f78e74a
Hassaan <98661403+hazzySoomro@users.noreply.github.com>
2023-04-07 04:25:41 +0500

    git push origin

--- components/Main.js
+++ components/Main.js
@@ -12,7 +12,7 @@ const Main = () => {
             LET&#39;S BUILD SOMETHING TOGETHER
           </p>
           <h1 className="py-4 text-gray-700">
-            Hi, I&#39;m <span className="text-[#5651e5]"> Hassaan ALi</span>
+            Hi, I&#39;m <span className="text-[#5651e5]"> Hassaan</span>
           </h1>
           <h1 className="py-2 text-gray-700">A Full-Stack Web Developer</h1>
           <p className="py-4 text-gray-600 text-sm tracking-widest sm:max-w-[70%] m-auto">
